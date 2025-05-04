9483f8308f031222522754052b72c80a5287d1db
Alexandr561 <Malxazmane@yandex.ru>
2025-05-04 03:33:17 +0300

    ??

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', [\App\Http\Controllers\PostController::class, 'index']);
+
